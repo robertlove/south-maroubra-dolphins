b9dd07ae005e8526e7b58764526ec7b5d1dc27e9
Robert Love <robertjohnlove@gmail.com>
2026-06-06 15:10:31 +1000

    Update purgecss.config.js

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   content: [
     './_site/**/*.html',
-    './_site/assets/js/script.js',
-    './_site/assets/js/bootstrap.bundle.min.js'
+    './_site/assets/js/**/*.js'
   ],
   css: [
     './_site/assets/css/style.css'
   ],
-  output: './_site/assets/css/style.css'
+  output: './_site/assets/css'
 }
